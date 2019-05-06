cd8358aefe111d8453feb33d09adbedb34fe5ad7
byl248 <48103780+byl248@users.noreply.github.com>
2019-05-06 18:08:06 -0400

    05.06.19

--- images.js
+++ images.js
@@ -1,9 +1,21 @@
-function enlarge(x) {
-	x.style.height = "60%";
-	x.style.width = "60%";
+const frame = document.getElementById("container");
+const images = document.querySelectorAll('img');
+
+function changeOpacity(event) {
+	if (event.target.tagName == 'IMG') {
+		for (let i = 0; i < images.length; i++) {
+			images[i].className = 'semitransparent';
+		}
+		event.target.className = "opaque";
+		event.target.addEventListener('mouseout', resetOpacity, false);
+	}
+	event.stopPropagation();
 }
 
-function revert(x) {
-	x.style.height = "50%";
-	x.style.width = "50%";
+function resetOpacity() {
+	for(let i = 0; i < images.length; i++) {
+		images[i].className = 'opaque';
+	}
 }
+
+frame.addEventListener('mouseover', changeOpacity, false);
